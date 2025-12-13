51215594776fc0555f19f3e8d8c11a66a1b43e46
seifbdair <saifahbdeer@gmail.com>
2025-12-13 21:04:45 +0300

    front

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://192.168.1.125:7035',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
 
